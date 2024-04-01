f0af24675ed53f4a4e2b4824dc15336aca7f1be9
SandstormNick <gibbensnick@gmail.com>
2024-04-01 21:14:14 +0200

    Added some components for the main section. Plus added some basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { WorkComponent } from './components/main/work/work.component';
+import { AboutComponent } from './components/main/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Work',
+        component: WorkComponent
+    },
+    {
+        path: 'about',
+        title: 'About',
+        component: AboutComponent
+    }
+];
