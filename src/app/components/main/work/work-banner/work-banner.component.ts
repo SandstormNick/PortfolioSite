6b93de7dd754eebaf24ad42794ec7bd56ab03c10
SandstormNick <gibbensnick@gmail.com>
2024-04-01 21:23:20 +0200

    Added WorkBanner component

---
+++ src/app/components/main/work/work-banner/work-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './work-banner.component.html',
+  styleUrl: './work-banner.component.scss'
+})
+export class WorkBannerComponent {
+
+}
--- src/app/components/main/work/work.component.ts
+++ src/app/components/main/work/work.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { WorkBannerComponent } from './work-banner/work-banner.component';
 
 @Component({
   selector: 'app-work',
   standalone: true,
-  imports: [],
+  imports: [WorkBannerComponent],
   templateUrl: './work.component.html',
   styleUrl: './work.component.scss'
 })
