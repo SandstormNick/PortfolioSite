3bed4488b216368fc664f616746294b51774261e
SandstormNick <gibbensnick@gmail.com>
2024-04-04 21:13:17 +0200

    Setting up the basic layout

--- src/app/components/main/about/about.component.ts
+++ src/app/components/main/about/about.component.ts
@@ -3,11 +3,12 @@ import { AboutBannerComponent } from './about-banner/about-banner.component';
 import { PastPresentFutureComponent } from './past-present-future/past-present-future.component';
 import { PrinciplesComponent } from './principles/principles.component';
 import { HobbiesComponent } from './hobbies/hobbies.component';
+import { PictureComponent } from './picture/picture.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [AboutBannerComponent, PastPresentFutureComponent, PrinciplesComponent, HobbiesComponent],
+  imports: [AboutBannerComponent, PictureComponent, PastPresentFutureComponent, PrinciplesComponent, HobbiesComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
