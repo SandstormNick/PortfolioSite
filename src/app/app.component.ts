2e0e759779cb65d1d7e4f1321e1076a20a5a8461
SandstormNick <gibbensnick@gmail.com>
2024-04-16 21:54:45 +0200

    Added keyboard shortcut to go home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/header/nav/nav.component';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NavComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, NavComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'SandstormNick.github.io';
+    constructor(private router: Router) { }
+
+    @HostListener('window:keydown', ['$event'])
+    handleShortcut(event: KeyboardEvent): void {
+        if (event.key === '/' || event.key === '?') {
+            this.router.navigate(['/']);
+        }
+    }
+
+    title = 'SandstormNick.github.io';
 }
