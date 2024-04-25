3dc4e44b3c21b60c552533dc83a425a35170f551
SandstormNick <gibbensnick@gmail.com>
2024-04-25 22:49:45 +0200

    Renamed componenet

--- src/app/components/main/about/about.component.ts
+++ src/app/components/main/about/about.component.ts
@@ -3,12 +3,12 @@ import { AboutBannerComponent } from './about-banner/about-banner.component';
 import { PastPresentFutureComponent } from './past-present-future/past-present-future.component';
 import { PrinciplesComponent } from './principles/principles.component';
 import { HobbiesComponent } from './hobbies/hobbies.component';
-import { PictureComponent } from './picture/picture.component';
+import { HeadShotComponent } from './head-shot/head-shot.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [AboutBannerComponent, PictureComponent, PastPresentFutureComponent, PrinciplesComponent, HobbiesComponent],
+  imports: [AboutBannerComponent, HeadShotComponent, PastPresentFutureComponent, PrinciplesComponent, HobbiesComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
