71e55be3fb1f854ff4e6b542cf71ef0293eb9fb3
SandstormNick <nick.gibbens@dyedurham.com>
2024-05-01 19:44:56 +0200

    Got the quotes section working quite nicely in the Hobbies section

--- src/app/components/main/about/hobbies/hobbies.component.ts
+++ src/app/components/main/about/hobbies/hobbies.component.ts
@@ -1,21 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Quote, quoteData } from './models/quote';
+import { CommonModule } from '@angular/common';
+import { interval, Subject, Subscription, takeUntil  } from 'rxjs';
+
 
 @Component({
   selector: 'app-hobbies',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './hobbies.component.html',
-  styleUrl: './hobbies.component.scss'
+  styleUrl: './hobbies.component.scss',
 })
-export class HobbiesComponent implements OnInit {
+export class HobbiesComponent implements OnInit, OnDestroy {
+    quoteSubscription: Subscription;
+    private _onDestroy = new Subject<void>();
+
     public quotes: Quote[];
-    
+
+    quotesLength: number;
+    currentQuote: string;
+    currentAuthor?: string;
+    quoteChanged: boolean;
+    intervalDuration = 5000;
+    timeoutDuration = this.intervalDuration - 2000;
 
     ngOnInit(): void {
         this.quotes = quoteData.map(quote => new Quote(quote.id, quote.quote, quote.author));
+        this.quotesLength = 0;
+        this.currentQuote = this.quotes[0].quote;
+        this.currentAuthor = this.quotes[0].author;
+        setTimeout(() => {
+            this.quoteChanged = true;
+        }, this.timeoutDuration);
 
-        console.log(this.quotes);
+        this.quoteSubscription = interval(this.intervalDuration).pipe(takeUntil(this._onDestroy)).subscribe((x => {
+            this.quotesLength++;
+            if (this.quotesLength === this.quotes.length){
+                this.quotesLength = 0;
+            }
+            this.nextQuote();
+        }));
+    }
+
+    nextQuote() : void {
+        setTimeout(() => {
+            this.quoteChanged = true;
+        }, this.timeoutDuration);
+
+        this.currentQuote = this.quotes[this.quotesLength].quote;
+        this.currentAuthor = this.quotes[this.quotesLength].author;
+
+        this.quoteChanged = false;
+    }
+
+    ngOnDestroy(): void {
+        this._onDestroy.next();
+        this._onDestroy.complete();
     }
 
 }
--- src/app/components/main/about/hobbies/models/quote.ts
+++ src/app/components/main/about/hobbies/models/quote.ts
@@ -12,7 +12,7 @@ export class Quote {
 
 export const quoteData = [
     {id: 1, quote: `Work hard in silence, let your success be your noise.`},
-    {id: 2, quote: `It's no use going back to yesterday, because I was different person then.`, author: `Lewis Carroll`},
+    {id: 2, quote: `It's no use going back to yesterday, because I was a different person then.`, author: `Lewis Carroll`},
     {id: 3, quote: `Experience is one thing you can't get for nothing.`, author: `Oscar Wilde`},
     {id: 4, quote: `If you take too long to decide what to do with your life, you'll find you've done it.` },
     {id: 5, quote: `The grass is greener where you water it.`, author: `Neil Barringham`},
