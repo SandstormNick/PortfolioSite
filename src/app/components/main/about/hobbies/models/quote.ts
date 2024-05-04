364c17f701bd6fba1fe284f0ad183831699e8667
SandstormNick <nick.gibbens@dyedurham.com>
2024-05-04 21:43:54 +0200

    Added the copy to the hobbies component

--- src/app/components/main/about/hobbies/models/quote.ts
+++ src/app/components/main/about/hobbies/models/quote.ts
@@ -17,4 +17,6 @@ export const quoteData = [
     {id: 4, quote: `If you take too long to decide what to do with your life, you'll find you've done it.` },
     {id: 5, quote: `The grass is greener where you water it.`, author: `Neil Barringham`},
     {id: 6, quote: `Show me a person who never made a mistake, and I will show you a person who never did anything.`, author: `William Rosenberg`},
+    {id: 7, quote: `Seek inspiration for it's a state of mind that can make a man divine.`, author: `Vishal Kapoor`},
+    {id: 8, quote: `Always go too far, because that's where you'll find the truth.`, author: `Albert Camus`},
 ]
