b45c01f753b611394d974517f7fb0c94829935aa
SandstormNick <nick.gibbens@dyedurham.com>
2024-05-06 22:29:50 +0200

    Created a stand alone quote looper component

--- src/app/components/main/about/hobbies/hobbies.component.ts
+++ src/app/components/main/about/hobbies/hobbies.component.ts
@@ -1,65 +1,26 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Quote, quoteData } from './models/quote';
+import { Component, OnInit } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
-import { interval, Subject, Subscription, takeUntil  } from 'rxjs';
 import { Photo, photoData } from './models/photo';
+import { QuoteLooperComponent } from './components/quote-looper/quote-looper.component';
 
 
 @Component({
   selector: 'app-hobbies',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [ CommonModule, QuoteLooperComponent ],
   templateUrl: './hobbies.component.html',
   styleUrl: './hobbies.component.scss',
 })
-export class HobbiesComponent implements OnInit, OnDestroy {
-    quoteSubscription: Subscription;
-    private _onDestroy = new Subject<void>();
+export class HobbiesComponent implements OnInit {
 
     public photos: Photo[];
     public photoPortrait: Photo;
     public photoLandscape: Photo;
 
-    public quotes: Quote[];
-    quotesLength: number;
-    currentQuote: string;
-    currentAuthor?: string;
-    quoteChanged: boolean;
-    intervalDuration = 5000;
-    timeoutDuration = this.intervalDuration - 2000;
-
     ngOnInit(): void {
         this.photos = photoData.map(photo => new Photo(photo.id, photo.src, photo.about, photo.isLandscape));
         this.setPhotos();
-
-
-        this.quotes = quoteData.map(quote => new Quote(quote.id, quote.quote, quote.author));
-        this.quotesLength = 0;
-        this.currentQuote = this.quotes[0].quote;
-        this.currentAuthor = this.quotes[0].author;
-        setTimeout(() => {
-            this.quoteChanged = true;
-        }, this.timeoutDuration);
-
-        this.quoteSubscription = interval(this.intervalDuration).pipe(takeUntil(this._onDestroy)).subscribe((x => {
-            this.quotesLength++;
-            if (this.quotesLength === this.quotes.length){
-                this.quotesLength = 0;
-            }
-            this.nextQuote();
-        }));
-    }
-
-    nextQuote() : void {
-        setTimeout(() => {
-            this.quoteChanged = true;
-        }, this.timeoutDuration);
-
-        this.currentQuote = this.quotes[this.quotesLength].quote;
-        this.currentAuthor = this.quotes[this.quotesLength].author;
-
-        this.quoteChanged = false;
     }
 
     setPhotos(): void {
@@ -76,10 +37,5 @@ export class HobbiesComponent implements OnInit, OnDestroy {
         const randomIndex = Math.floor(Math.random() * array.length);
         return array[randomIndex];
       }
-
-    ngOnDestroy(): void {
-        this._onDestroy.next();
-        this._onDestroy.complete();
-    }
 
 }
