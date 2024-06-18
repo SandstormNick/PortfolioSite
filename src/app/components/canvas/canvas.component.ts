568f97f3233905f3bef76a5e456a09a66355d54d
SandstormNick <nick.gibbens@dyedurham.com>
2024-06-18 21:59:28 +0200

    Move the gradient colors into their own method

--- src/app/components/canvas/canvas.component.ts
+++ src/app/components/canvas/canvas.component.ts
@@ -38,29 +38,40 @@ export class CanvasComponent implements OnInit, AfterViewInit {
         const height = this.canvas.nativeElement.height;
 
         // Create a linear gradient
-        const gradient = this.ctx.createLinearGradient(0, 0, 0, height);
+        let gradient = this.ctx.createLinearGradient(0, 0, 0, height);
 
         // Define gradient colors
         if (this.isLightTheme) {
-            gradient.addColorStop(0, 'darkgreen');
-            gradient.addColorStop(0.15, 'green');
-            gradient.addColorStop(0.3, 'greenyellow');
-            gradient.addColorStop(0.45, 'lightgreen');
-            gradient.addColorStop(0.7, 'palegreen');
-            gradient.addColorStop(1, 'hsla(36, 31%, 90%, 1)'); // Add the site background color
+            gradient = this.setLightThemeColors(gradient);
         } else {
-            // Define dark theme gradient colors
-            gradient.addColorStop(0, 'darkolivegreen');
-            gradient.addColorStop(0.15, 'olivedrab');
-            gradient.addColorStop(0.3, 'yellowgreen');
-            gradient.addColorStop(0.45, 'mediumseagreen');
-            gradient.addColorStop(0.7, 'seagreen');
-            gradient.addColorStop(1, 'hsla(158, 23%, 18%, 1)'); // Site background color for dark theme
+            gradient = this.setDarkThemeColors(gradient);
         }
 
         // Apply the gradient as fill style
         this.ctx.fillStyle = gradient;
         this.ctx.fillRect(0, 0, width, height);
+    }
+
+    private setLightThemeColors(gradient: CanvasGradient): CanvasGradient {
+        gradient.addColorStop(0, 'darkgreen');
+        gradient.addColorStop(0.15, 'green');
+        gradient.addColorStop(0.3, 'greenyellow');
+        gradient.addColorStop(0.45, 'lightgreen');
+        gradient.addColorStop(0.7, 'palegreen');
+        gradient.addColorStop(1, 'hsla(36, 31%, 90%, 1)'); // Add the site background color
+
+        return gradient;
+    }
+
+    private setDarkThemeColors(gradient: CanvasGradient): CanvasGradient {
+        gradient.addColorStop(0, 'darkolivegreen');
+        gradient.addColorStop(0.15, 'olivedrab');
+        gradient.addColorStop(0.3, 'yellowgreen');
+        gradient.addColorStop(0.45, 'mediumseagreen');
+        gradient.addColorStop(0.7, 'seagreen');
+        gradient.addColorStop(1, 'hsla(158, 23%, 18%, 1)'); // Site background color for dark theme
+
+        return gradient;
     }
 
 }
