bff5d48661ec9648c702d8c86d89da65d7404e40
SandstormNick <gibbensnick@gmail.com>
2024-09-02 20:27:29 +0200

    Added link to Tracker app in footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSquareGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+import { faChartSimple } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-footer',
@@ -14,8 +15,9 @@ export class FooterComponent {
     faGithub = faSquareGithub;
     faLinkedin = faLinkedin;
     faCopyright = faCopyright;
+    faChartSimple = faChartSimple;
 
     githubLink = 'https://github.com/SandstormNick';
     linkedLink = 'https://www.linkedin.com/in/nicholas-gibbens-10b191174/';
-
+    trackerLink = 'https://wonderful-sand-0119aa41e.5.azurestaticapps.net/';
 }
